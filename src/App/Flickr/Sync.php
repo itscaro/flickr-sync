2c7992233d5996cc01eacc644d60281548392cc5
Minh-Quan TRAN <account@itscaro.me>
2014-11-06 23:06:25 +0000

    update

--- src/App/Flickr/Sync.php
+++ src/App/Flickr/Sync.php
@@ -10,29 +10,46 @@ use Symfony\Component\Console\Question;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOException;
 
-class Sync extends Command
-{
+class Sync extends Command {
+
+    /**
+     *
+     * @var \Symfony\Component\Console\Input\Input
+     */
+    protected $_input;
+
+    /**
+     *
+     * @var \Symfony\Component\Console\Output\Output 
+     */
+    protected $_output;
 
     protected function configure()
     {
         $this->setName("flickr:sync")
-            ->setDescription("")
-            ->setDefinition(array())
-            ->setHelp(<<<EOT
+                ->setDescription("")
+                ->setDefinition(array())
+                ->setHelp(<<<EOT
 EOT
-        );
+                )->addOption('dry-run', 'd', \Symfony\Component\Console\Input\InputOption::VALUE_OPTIONAL, 'Dry run, do not upload to Flickr', false)
+                ->addArgument('directory', InputArgument::OPTIONAL, 'Directory to scan', getcwd());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->_input = $input;
+        $this->_output = $output;
+
+        $app = $this->getApplication();
+        /* @var $app \Itscaro\App\Application */
+        $config = $app->getConfig();
+
         $settings = $this->_getDataStore();
-        $api_key = "ef41e52674a4572bd9c05c1df0a3c9a4";
-        $api_secret = "077c25b4411d432c";
 
         $configOauth = array(
             'siteUrl' => 'https://www.flickr.com/services/oauth/',
-            'consumerKey' => 'ef41e52674a4572bd9c05c1df0a3c9a4',
-            'consumerSecret' => '077c25b4411d432c',
+            'consumerKey' => $config['flickr']['oauth']['consumerKey'],
+            'consumerSecret' => $config['flickr']['oauth']['consumerSecret'],
         );
         $configHttpClient = array(
             'adapter' => 'Zend\Http\Client\Adapter\Curl',
@@ -52,14 +69,14 @@ EOT
             $requestToken = $consumer->getRequestToken();
             //var_dump($requestToken->getToken());
 
-            echo "Please open this URL: \n";
-            echo $consumer->getRedirectUrl(array(
+            $output->writeln("<info>Please open this URL:</info>");
+            $authorizationUrl = $consumer->getRedirectUrl(array(
                 'perms' => 'delete'
             ));
-            echo "\n";
+            $output->writeln("<info>" . $authorizationUrl . "</info>");
 
             $helper = $this->getHelper('question');
-            $question = new Question\Question('Confirmation code:');
+            $question = new Question\Question('<question>Confirmation code:</question>');
             $response = $helper->ask($input, $output, $question);
 
             if ($response) {
@@ -77,15 +94,14 @@ EOT
                 'accessToken' => $accessToken
             ));
         } else {
-            $flickr = new \Itscaro\Service\Flickr\Flickr($configOauth, $configHttpClient);
-            $flickr->setAccessToken($settings['accessToken']);
-
+//            $flickr = new \Itscaro\Service\Flickr\Flickr($configOauth, $configHttpClient);
+//            $flickr->setAccessToken($settings['accessToken']);
+//
             $flickrMulti = new \Itscaro\Service\Flickr\ClientMulti('https://api.flickr.com/services/rest', $configOauth, $configHttpClient);
             $flickrMulti->setAccessToken($settings['accessToken']);
 
-            $flickrSimple = new \Itscaro\Service\Flickr\Client('https://api.flickr.com/services/rest', $configOauth, $configHttpClient);
-            $flickrSimple->setAccessToken($settings['accessToken']);
-
+//            $flickrSimple = new \Itscaro\Service\Flickr\Client('https://api.flickr.com/services/rest', $configOauth, $configHttpClient);
+//            $flickrSimple->setAccessToken($settings['accessToken']);
 //            $result = $flickrSimple->get('flickr.photosets.getList', array(
 //                'user_id' => "10995091@N00"
 //            ));
@@ -101,35 +117,98 @@ EOT
 //            $result = $flickr->dispatch();
 //            var_dump($result);
 
+            $finder = $this->_scan($this->_input->getArgument('directory'));
+
             $flickrUploader = new \Itscaro\Service\Flickr\Photo($settings['accessToken'], $configOauth, $configHttpClient);
-            $file = ROOTDIR . '/data/useravatar.png';
 
-            $id = $flickrUploader->uploadSync($file);
-            echo $file . " - " . $id . "\n";
+            $errors = array();
+            $filesBatch = array();
+            $filesFound = count($finder);
+            $counter = 0;
+            foreach ($finder as $file) {
+                $counter++;
+                /* @var $file \Symfony\Component\Finder\SplFileInfo */
 
-            $id = $flickrUploader->uploadAsync($file);
-            echo $file . " - " . $id . "\n";
+                $filesBatch[] = $file;
 
+                if (count($filesBatch) == 10 || $filesFound == $counter) {
+
+                    $errors += $this->_process($flickrMulti, $flickrUploader, $filesBatch);
+                    $filesBatch = array();
+                }
+            }
+
+//            $id = $flickrUploader->uploadSync($filePath, basename($file), null, "itscaro:app=flickr-sync,itscaro:photo_hash=".  md5_file($file));
+//            echo $file . " - " . $id . "\n";
 //            $params = $this->_prepareParamsForProcess('by-taken-year', $settings['accessToken']->getParam('user_nsid'), 1);
 //            $response = $flickrMulti->dispatch('GET', 'flickr.photosets.getPhotos', $params);
 //
 //            var_dump($response);
         }
 
-        echo "\n\nDone\n";
+        $output->writeln('<info>Done</info>');
     }
 
     protected function _scan($dir)
     {
         $finder = new \Symfony\Component\Finder\Finder();
         $finder->in($dir)
-            ->name('\.(jpg|jpeg|png|gif|tif|tiff)$');
+                ->name('/.*\.(jpg|jpeg|png|gif|tif|tiff)$/');
 
-        foreach ($finder as $file) {
-            /* @var $file \Symfony\Component\Finder\SplFileInfo */
-            $file->getBasename();
-            
+        return $finder;
+    }
+
+    protected function _process($flickrMulti, $flickrUploader, array $files)
+    {
+        $errors = array();
+        $filesInfo = array();
+        foreach ($files as $file) {
+            $filePath = $file->getRealPath();
+            $filesInfo[$filePath]['hash'] = md5_file($filePath);
+            $filesInfo[$filePath]['requestId'] = $flickrMulti->addToQueue('GET', 'flickr.photos.search', array(
+                'user_id' => "10995091@N00",
+                "machine_tags" => "itscaro:app=flickr-sync,itscaro:photo_hash=" . $filesInfo[$filePath]['hash'],
+                "machine_tag_mode" => "all"
+            ));
         }
+
+        if ($this->_output->isVerbose()) {
+            $this->_output->writeln('>>> Files to check');
+            $this->_output->writeln(var_export($filesInfo, 1));
+        }
+
+        $result = $flickrMulti->dispatchMulti();
+
+        if ($this->_output->isVeryVerbose()) {
+            $this->_output->writeln('>>> flickr.photos.search');
+            $this->_output->writeln(var_export($result, 1));
+        }
+
+        foreach ($files as $file) {
+            $filePath = $file->getRealPath();
+
+            $requestId = $filesInfo[$filePath]['requestId'];
+            if (isset($result[$requestId])) {
+                $_result = json_decode($result[$requestId], true);
+                if ($_result['photos']['total'] == 0) {
+                    // File not found on Flickr
+                    $tag = "itscaro:app=flickr-sync itscaro:photo_hash=" . $filesInfo[$filePath]['hash'];
+
+                    if ($this->_input->getOption('dry-run')) {
+                        $id = $flickrUploader->uploadAsync($filePath, $file->getBasename(), $file->getPath(), $tag);
+                        $this->_output->writeln("<comment>File uploaded: {$filePath} (Photo ID: {$id})</comment>");
+                    }
+                } else {
+                    // File exists on Flickr
+                    $this->_output->writeln("File exists already: {$filePath}");
+                }
+            } else {
+                $errors[] = $file;
+                $this->_output->writeln("<error>Could not verify {$filePath}</error>");
+            }
+        }
+
+        return $errors;
     }
 
     protected function _getDataStore()
